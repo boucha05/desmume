boucha05/desmume

commit 79c8cff87555f4e16e109d10c005fc186332f55e

GPU: - Do a whole bunch of code cleanup.
git-svn-id: https://svn.code.sf.net/p/desmume/code/trunk@5198 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/render3D.cpp
@@ -210,9 +210,15 @@ Render3DError Render3D::EndRender(const u64 frameCount)
 	return RENDER3DERROR_NOERR;
 }
 
-Render3DError Render3D::FlushFramebuffer(FragmentColor *dstBuffer)
+Render3DError Render3D::FlushFramebuffer(FragmentColor *dstRGBA6665, u16 *dstRGBA5551)
 {
-	memcpy(dstBuffer, this->_framebufferColor, this->_framebufferColorSizeBytes);
+	memcpy(dstRGBA6665, this->_framebufferColor, this->_framebufferColorSizeBytes);
+	
+	for (size_t i = 0; i < (this->_framebufferWidth * this->_framebufferHeight); i++)
+	{
+		dstRGBA5551[i] = R6G6B6TORGB15(this->_framebufferColor[i].r, this->_framebufferColor[i].g, this->_framebufferColor[i].b) | ((this->_framebufferColor[i].a == 0) ? 0x0000 : 0x8000);
+	}
+	
 	return RENDER3DERROR_NOERR;
 }
 
@@ -369,7 +375,7 @@ Render3DError Render3D::Render(const GFX3D &engine)
 
 Render3DError Render3D::RenderFinish()
 {
-	this->FlushFramebuffer(gfx3d_convertedScreen);
+	this->FlushFramebuffer(gfx3d_colorRGBA6665, gfx3d_colorRGBA5551);
 	return RENDER3DERROR_NOERR;
 }
 

desmume/src/GPU.h
@@ -583,6 +583,23 @@ enum BGType {
 	BGType_AffineExt=4, BGType_AffineExt_256x16=5, BGType_AffineExt_256x1=6, BGType_AffineExt_Direct=7
 };
 
+enum GPUDisplayMode
+{
+	GPUDisplayMode_Off				= 0,
+	GPUDisplayMode_Normal			= 1,
+	GPUDisplayMode_VRAM				= 2,
+	GPUDisplayMode_MainMemory		= 3
+};
+
+enum GPUMasterBrightMode
+{
+	GPUMasterBrightMode_Disable		= 0,
+	GPUMasterBrightMode_Up			= 1,
+	GPUMasterBrightMode_Down		= 2,
+	GPUMasterBrightMode_Reserved	= 3
+	
+};
+
 extern const BGType GPU_mode2type[8][4];
 
 struct GPU
@@ -636,7 +653,7 @@ struct GPU
 
 	u8 core;
 
-	u8 dispMode;
+	GPUDisplayMode dispMode;
 	u8 vramBlock;
 	u8 *VRAMaddr;
 
@@ -685,9 +702,9 @@ struct GPU
 	bool blend2[8];
 
 	CACHE_ALIGN u16 tempScanlineBuffer[256];
-	u8 *tempScanline;
+	u16 *tempScanline;
 
-	u8	MasterBrightMode;
+	GPUMasterBrightMode	MasterBrightMode;
 	u32 MasterBrightFactor;
 
 	CACHE_ALIGN u8 bgPixels[1024]; //yes indeed, this is oversized. map debug tools try to write to it
@@ -695,11 +712,8 @@ struct GPU
 	u32 currLine;
 	u8 currBgNum;
 	bool blend1;
-	u8* currDst;
-
-	FragmentColor *_3dColorLine;
-
-
+	u16 *currDst;
+	
 	static struct MosaicLookup {
 
 		struct TableEntry {
@@ -728,7 +742,7 @@ struct GPU
 	FORCEINLINE FASTCALL bool _master_setFinalBGColor(u16 &color, const u32 x);
 
 	template<BlendFunc FUNC, bool WINDOW>
-	FORCEINLINE FASTCALL void _master_setFinal3dColor(int dstX, int srcX);
+	FORCEINLINE FASTCALL void _master_setFinal3dColor(int dstX, const FragmentColor src);
 
 	int setFinalColorBck_funcNum;
 	int bgFunc;
@@ -751,7 +765,7 @@ struct GPU
 	}
 
 
-	void setFinalColor3d(int dstX, int srcX);
+	void setFinalColor3d(int dstX, const FragmentColor src);
 	
 	template<bool BACKDROP, int FUNCNUM> void setFinalColorBG(u16 color, const u32 x);
 	template<bool MOSAIC, bool BACKDROP> FORCEINLINE void __setFinalColorBck(u16 color, const u32 x, const int opaque);
@@ -824,12 +838,14 @@ static void REG_DISPx_pack_test(GPU * gpu)
 }
 #endif
 
-CACHE_ALIGN extern u8 GPU_screen[4*256*192];
-
+extern u16 *GPU_screen;
 
 GPU * GPU_Init(u8 l);
 void GPU_Reset(GPU *g, u8 l);
 void GPU_DeInit(GPU *);
+size_t GPU_GetFramebufferWidth();
+size_t GPU_GetFramebufferHeight();
+void GPU_SetFramebufferSize(size_t w, size_t h);
 
 //these are functions used by debug tools which want to render layers etc outside the context of the emulation
 namespace GPU_EXT

desmume/src/NDSSystem.cpp
@@ -161,8 +161,6 @@ int NDS_Init()
 		NDS_RunAdvansceneAutoImport();
 	}
 	
-	gfx3d_init();
-
 	armcpu_new(&NDS_ARM9,0);
 	NDS_ARM9.SetBaseMemoryInterface(&arm9_base_memory_iface);
 	NDS_ARM9.SetBaseMemoryInterfaceData(NULL);
@@ -190,8 +188,6 @@ void NDS_DeInit(void)
 	SPU_DeInit();
 	Screen_DeInit();
 	MMU_DeInit();
-	gfx3d_deinit();
-
 	WIFI_DeInit();
 	
 	delete cheats;

desmume/src/OGLRender.cpp
@@ -72,6 +72,7 @@ void ENDGL()
 bool (*oglrender_init)() = NULL;
 bool (*oglrender_beginOpenGL)() = NULL;
 void (*oglrender_endOpenGL)() = NULL;
+bool (*oglrender_framebufferDidResizeCallback)(size_t w, size_t h) = NULL;
 void (*OGLLoadEntryPoints_3_2_Func)() = NULL;
 void (*OGLCreateRenderer_3_2_Func)(OpenGLRenderer **rendererPtr) = NULL;
 
@@ -884,7 +885,7 @@ void OpenGLRenderer::SetVersion(unsigned int major, unsigned int minor, unsigned
 	this->versionRevision = revision;
 }
 
-Render3DError OpenGLRenderer::FlushFramebuffer(FragmentColor *dstBuffer)
+Render3DError OpenGLRenderer::FlushFramebuffer(FragmentColor *dstRGBA6665, u16 *dstRGBA5551)
 {
 	// Convert from 32-bit BGRA8888 format to 32-bit RGBA6665 reversed format. OpenGL
 	// stores pixels using a flipped Y-coordinate, so this needs to be flipped back
@@ -896,10 +897,17 @@ Render3DError OpenGLRenderer::FlushFramebuffer(FragmentColor *dstBuffer)
 			// Use the correct endian format since OpenGL uses the native endian of
 			// the architecture it is running on.
 #ifdef WORDS_BIGENDIAN
-			dstBuffer[iw].color = BGRA8888_32_To_RGBA6665_32(this->_framebufferColor[ir].color);
-			
+			dstRGBA6665[iw].color = BGRA8888_32_To_RGBA6665_32(this->_framebufferColor[ir].color);
+			dstRGBA5551[iw] = R5G5B5TORGB15((this->_framebufferColor[ir].b >> 3) & 0x1F,
+											(this->_framebufferColor[ir].g >> 3) & 0x1F,
+											(this->_framebufferColor[ir].r >> 3) & 0x1F) |
+											((this->_framebufferColor[ir].a == 0) ? 0x0000 : 0x8000);
 #else
-			dstBuffer[iw].color = BGRA8888_32Rev_To_RGBA6665_32Rev(this->_framebufferColor[ir].color);
+			dstRGBA6665[iw].color = BGRA8888_32Rev_To_RGBA6665_32Rev(this->_framebufferColor[ir].color);
+			dstRGBA5551[iw] = R5G5B5TORGB15((this->_framebufferColor[ir].b >> 3) & 0x1F,
+											(this->_framebufferColor[ir].g >> 3) & 0x1F,
+											(this->_framebufferColor[ir].r >> 3) & 0x1F) |
+											((this->_framebufferColor[ir].a == 0) ? 0x0000 : 0x8000);
 #endif
 		}
 	}
@@ -1405,7 +1413,7 @@ Render3DError OpenGLRenderer_1_2::CreateFBOs()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_NONE);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, GFX3D_FRAMEBUFFER_WIDTH, GFX3D_FRAMEBUFFER_HEIGHT, 0, GL_DEPTH_STENCIL_EXT, GL_UNSIGNED_INT_24_8_EXT, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8_EXT, GFX3D_FRAMEBUFFER_WIDTH, GFX3D_FRAMEBUFFER_HEIGHT, 0, GL_DEPTH_STENCIL_EXT, GL_UNSIGNED_INT_24_8_EXT, NULL);
 	
 	glBindTexture(GL_TEXTURE_2D, OGLRef.texCIDepthID);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
@@ -2682,7 +2690,7 @@ Render3DError OpenGLRenderer_1_2::RenderFinish()
 	
 	ENDGL();
 	
-	this->FlushFramebuffer(gfx3d_convertedScreen);
+	this->FlushFramebuffer(gfx3d_colorRGBA6665, gfx3d_colorRGBA5551);
 	this->_pixelReadNeedsFinish = false;
 	
 	return OGLERROR_NOERR;
@@ -2701,6 +2709,11 @@ Render3DError OpenGLRenderer_1_2::SetFramebufferSize(size_t w, size_t h)
 	this->_framebufferHeight = h;
 	this->_framebufferColorSizeBytes = w * h * sizeof(FragmentColor);
 	this->_framebufferColor = (FragmentColor *)realloc(this->_framebufferColor, this->_framebufferColorSizeBytes);
+	
+	if (oglrender_framebufferDidResizeCallback != NULL)
+	{
+		oglrender_framebufferDidResizeCallback(w, h);
+	}
 	
 	if (this->isFBOSupported)
 	{
@@ -2838,6 +2851,11 @@ Render3DError OpenGLRenderer_1_3::SetFramebufferSize(size_t w, size_t h)
 	this->_framebufferHeight = h;
 	this->_framebufferColorSizeBytes = w * h * sizeof(FragmentColor);
 	this->_framebufferColor = (FragmentColor *)realloc(this->_framebufferColor, this->_framebufferColorSizeBytes);
+	
+	if (oglrender_framebufferDidResizeCallback != NULL)
+	{
+		oglrender_framebufferDidResizeCallback(w, h);
+	}
 	
 	if (this->isFBOSupported)
 	{
@@ -3211,7 +3229,7 @@ Render3DError OpenGLRenderer_1_5::RenderFinish()
 	
 	ENDGL();
 	
-	this->FlushFramebuffer(gfx3d_convertedScreen);
+	this->FlushFramebuffer(gfx3d_colorRGBA6665, gfx3d_colorRGBA5551);
 	this->_pixelReadNeedsFinish = false;
 	
 	return OGLERROR_NOERR;
@@ -3994,7 +4012,7 @@ Render3DError OpenGLRenderer_2_1::RenderFinish()
 	
 	ENDGL();
 	
-	this->FlushFramebuffer(gfx3d_convertedScreen);
+	this->FlushFramebuffer(gfx3d_colorRGBA6665, gfx3d_colorRGBA5551);
 	this->_pixelReadNeedsFinish = false;
 	
 	return OGLERROR_NOERR;

desmume/src/OGLRender.h
@@ -531,6 +531,9 @@ extern bool (*oglrender_beginOpenGL)();
 //This is called by OGLRender after it is done using opengl.
 extern void (*oglrender_endOpenGL)();
 
+//This is called by OGLRender whenever the framebuffer is resized.
+extern bool (*oglrender_framebufferDidResizeCallback)(size_t w, size_t h);
+
 // Helper functions for calling the above function pointers at the
 // beginning and ending of OpenGL commands.
 bool BEGINGL();
@@ -575,7 +578,7 @@ protected:
 	bool _pixelReadNeedsFinish;
 	size_t _currentPolyIndex;
 	
-	Render3DError FlushFramebuffer(FragmentColor *dstBuffer);
+	Render3DError FlushFramebuffer(FragmentColor *dstRGBA6665, u16 *dstRGBA5551);
 	
 	// OpenGL-specific methods
 	virtual Render3DError CreateVBOs() = 0;

desmume/src/OGLRender_3_2.cpp
@@ -1564,6 +1564,11 @@ Render3DError OpenGLRenderer_3_2::SetFramebufferSize(size_t w, size_t h)
 	this->_framebufferColorSizeBytes = w * h * sizeof(FragmentColor);
 	this->_framebufferColor = (FragmentColor *)realloc(this->_framebufferColor, this->_framebufferColorSizeBytes);
 	
+	if (oglrender_framebufferDidResizeCallback != NULL)
+	{
+		oglrender_framebufferDidResizeCallback(w, h);
+	}
+	
 	glActiveTexture(GL_TEXTURE0 + OGLTextureUnitID_GColor);
 	glBindTexture(GL_TEXTURE_2D, OGLRef.texGDepthStencilID);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, w, h, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);

desmume/src/gfx3d.cpp
@@ -310,7 +310,8 @@ static float normalTable[1024];
 #define fix10_2float(v) (((float)((s32)(v))) / (float)(1<<9))
 
 // Color buffer that is filled by the 3D renderer and is read by the GPU engine.
-FragmentColor *gfx3d_convertedScreen = NULL;
+FragmentColor *gfx3d_colorRGBA6665 = NULL;
+u16 *gfx3d_colorRGBA5551 = NULL;
 static size_t gfx3d_framebufferWidth = GFX3D_FRAMEBUFFER_WIDTH;
 static size_t gfx3d_framebufferHeight = GFX3D_FRAMEBUFFER_HEIGHT;
 
@@ -565,8 +566,11 @@ void gfx3d_deinit()
 	vertlists = NULL;
 	vertlist = NULL;
 	
-	free(gfx3d_convertedScreen);
-	gfx3d_convertedScreen = NULL;
+	free(gfx3d_colorRGBA6665);
+	gfx3d_colorRGBA6665 = NULL;
+	
+	free(gfx3d_colorRGBA5551);
+	gfx3d_colorRGBA5551 = NULL;
 }
 
 void gfx3d_reset()
@@ -641,7 +645,8 @@ void gfx3d_reset()
 	last_s = 0;
 	viewport = 0xBFFF0000;
 
-	memset(gfx3d_convertedScreen, 0, sizeof(gfx3d_framebufferWidth * gfx3d_framebufferHeight * sizeof(FragmentColor)));
+	memset(gfx3d_colorRGBA6665, 0, gfx3d_framebufferWidth * gfx3d_framebufferHeight * sizeof(FragmentColor));
+	memset(gfx3d_colorRGBA5551, 0, gfx3d_framebufferWidth * gfx3d_framebufferHeight * sizeof(u16));
 
 	gfx3d.state.clearDepth = DS_DEPTH15TO24(0x7FFF);
 	
@@ -673,14 +678,15 @@ void gfx3d_setFramebufferSize(size_t w, size_t h)
 	
 	// Check if we're calling this function from initialization.
 	// If we're not initializing, we need to finish rendering first.
-	if (gfx3d_convertedScreen != NULL)
+	if (gfx3d_colorRGBA6665 != NULL && gfx3d_colorRGBA5551 != NULL)
 	{
 		CurrentRenderer->RenderFinish();
 	}
 	
 	gfx3d_framebufferWidth = w;
 	gfx3d_framebufferHeight = h;
-	gfx3d_convertedScreen = (FragmentColor *)realloc(gfx3d_convertedScreen, w * h * sizeof(FragmentColor));
+	gfx3d_colorRGBA6665 = (FragmentColor *)realloc(gfx3d_colorRGBA6665, w * h * sizeof(FragmentColor));
+	gfx3d_colorRGBA5551 = (u16 *)realloc(gfx3d_colorRGBA5551, w * h * sizeof(u16));
 	
 	CurrentRenderer->SetFramebufferSize(w, h);
 }
@@ -2345,7 +2351,7 @@ void gfx3d_VBlankEndSignal(bool skipFrame)
 
 	if (!CommonSettings.showGpu.main)
 	{
-		memset(gfx3d_convertedScreen, 0, sizeof(gfx3d_framebufferWidth * gfx3d_framebufferHeight * sizeof(FragmentColor)));
+		memset(gfx3d_colorRGBA6665, 0, sizeof(gfx3d_framebufferWidth * gfx3d_framebufferHeight * sizeof(FragmentColor)));
 		return;
 	}
 	
@@ -2457,24 +2463,16 @@ void gfx3d_glGetLightColor(const size_t index, u32 &dst)
 	dst = lightColor[index];
 }
 
-void gfx3d_GetLineData(const size_t line, FragmentColor **dst)
+const FragmentColor* gfx3d_GetLineDataRGBA6665(const size_t line)
 {
 	CurrentRenderer->RenderFinish();
-	*dst = gfx3d_convertedScreen + (line * gfx3d_framebufferWidth);
+	return (gfx3d_colorRGBA6665 + (line * gfx3d_framebufferWidth));
 }
 
-void gfx3d_GetLineData15bpp(const size_t line, u16 **dst)
+const u16* gfx3d_GetLineDataRGBA5551(const size_t line)
 {
-	//TODO - this is not very thread safe!!!
-	static u16 buf[GFX3D_FRAMEBUFFER_WIDTH];
-	*dst = buf;
-	
-	FragmentColor *lineData;
-	gfx3d_GetLineData(line, &lineData);
-	for (size_t i = 0; i < GFX3D_FRAMEBUFFER_WIDTH; i++)
-	{
-		buf[i] = R6G6B6TORGB15(lineData[i].r, lineData[i].g, lineData[i].b) | ((lineData[i].a == 0) ? 0x0000 : 0x8000);
-	}
+	CurrentRenderer->RenderFinish();
+	return (gfx3d_colorRGBA5551 + (line * gfx3d_framebufferWidth));
 }
 
 
@@ -2564,7 +2562,7 @@ SFORMAT SF_GFX3D[]={
 	{ "GTVC", 4, 1, &tempVertInfo.count},
 	{ "GTVM", 4, 4, tempVertInfo.map},
 	{ "GTVF", 4, 1, &tempVertInfo.first},
-	{ "G3CX", 1, 4*GFX3D_FRAMEBUFFER_WIDTH*GFX3D_FRAMEBUFFER_HEIGHT, gfx3d_convertedScreen},
+	{ "G3CX", 1, 4*GFX3D_FRAMEBUFFER_WIDTH*GFX3D_FRAMEBUFFER_HEIGHT, gfx3d_colorRGBA6665},
 	{ 0 }
 };
 

commit 3e2b4b9b8e20a8d6588adcdf79cd9c921dbc5483

Render3D: - Fix bug where using the Null 3D renderer would greatly reduce performance, instead of improving performance as intended. (Regression from r5198.)
git-svn-id: https://svn.code.sf.net/p/desmume/code/trunk@5205 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/render3D.cpp
@@ -331,6 +331,7 @@ Render3DError Render3D::Reset()
 	if (this->_framebufferColor != NULL)
 	{
 		memset(this->_framebufferColor, 0, this->_framebufferColorSizeBytes);
+		this->FlushFramebuffer(gfx3d_colorRGBA6665, gfx3d_colorRGBA5551);
 	}
 	
 	memset(this->clearImageColor16Buffer, 0, sizeof(this->clearImageColor16Buffer));
@@ -375,7 +376,6 @@ Render3DError Render3D::Render(const GFX3D &engine)
 
 Render3DError Render3D::RenderFinish()
 {
-	this->FlushFramebuffer(gfx3d_colorRGBA6665, gfx3d_colorRGBA5551);
 	return RENDER3DERROR_NOERR;
 }
 