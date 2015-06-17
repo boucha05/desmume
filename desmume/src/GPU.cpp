boucha05/desmume

commit abb5e2fbc60c5ad1c15442d6be906a1b85ad4b7f

GPU: - More code cleanup, and various small optimizations. - Optimize the OpenGL 3D renderer’s framebuffer flush. (Requires SSSE3.)
git-svn-id: https://svn.code.sf.net/p/desmume/code/trunk@5208 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/GPU.h
@@ -32,6 +32,9 @@ struct MMU_struct;
 //#undef FORCEINLINE
 //#define FORCEINLINE
 
+#define GPU_FRAMEBUFFER_NATIVE_WIDTH	256
+#define GPU_FRAMEBUFFER_NATIVE_HEIGHT	192
+
 void gpu_savestate(EMUFILE* os);
 bool gpu_loadstate(EMUFILE* is, int size);
 
@@ -544,14 +547,14 @@ struct _OAM_
 	u16 attr3;
 };
 
-void SlurpOAM(_OAM_* oam_output, void* oam_buffer, int oam_index);
-u16 SlurpOAMAffineParam(void* oam_buffer, int oam_index);
+void SlurpOAM(_OAM_ *oam_output, void *oam_buffer, const size_t oam_index);
+u16 SlurpOAMAffineParam(void *oam_buffer, const size_t oam_index);
 
 typedef struct
 {
 	 s16 x;
 	 s16 y;
-} size;
+} SpriteSize;
 
 
 #define NB_PRIORITIES	4
@@ -620,7 +623,7 @@ struct GPU
 
 	_BGxCNT & bgcnt(int num) { return (dispx_st)->dispx_BGxCNT[num].bits; }
 	_DISPCNT & dispCnt() { return dispx_st->dispx_DISPCNT.bits; }
-	template<bool MOSAIC> void modeRender(int layer);
+	template<bool MOSAIC> void modeRender(const size_t layer);
 
 	DISPCAPCNT dispCapCnt;
 	BOOL LayersEnable[5];
@@ -647,18 +650,18 @@ struct GPU
 	} mosaicColors;
 
 	u8 sprNum[256];
-	//u8 h_win[2][256];
 	u8 *h_win[2];
 	const u8 *curr_win[2];
-	void update_winh(int WIN_NUM); 
 	bool need_update_winh[2];
 	
-	template<int WIN_NUM> void setup_windows();
+	template<size_t WIN_NUM> void update_winh();
+	template<size_t WIN_NUM> void setup_windows();
 
 	GPUCoreID core;
 	GPUDisplayMode dispMode;
 	u8 vramBlock;
-	u8 *VRAMaddr;
+	u16 *VRAMaddr;
+	u16 *VRAMBuffer;
 
 	//FIFO	fifo;
 
@@ -714,7 +717,6 @@ struct GPU
 	GPUMasterBrightMode	MasterBrightMode;
 	u32 MasterBrightFactor;
 
-	//CACHE_ALIGN u8 bgPixels[1024]; //yes indeed, this is oversized. map debug tools try to write to it
 	u8 *bgPixels;
 
 	u32 currLine;
@@ -779,9 +781,9 @@ struct GPU
 	template<bool MOSAIC, bool BACKDROP> FORCEINLINE void __setFinalColorBck(u16 color, const size_t x, const bool opaque);
 	template<bool MOSAIC, bool BACKDROP, int FUNCNUM> FORCEINLINE void ___setFinalColorBck(u16 color, const size_t x, const bool opaque);
 
-	void setAffineStart(int layer, int xy, u32 val);
-	void setAffineStartWord(int layer, int xy, u16 val, int word);
-	u32 getAffineStart(int layer, int xy);
+	void setAffineStart(const size_t layer, int xy, u32 val);
+	void setAffineStartWord(const size_t layer, int xy, u16 val, int word);
+	u32 getAffineStart(const size_t layer, int xy);
 	void refreshAffineStartRegs(const int num, const int xy);
 
 	struct AffineInfo {
@@ -814,8 +816,8 @@ struct GPU
 		updateBLDALPHA();
 	}
 
-	u32 getHOFS(int bg);
-	u32 getVOFS(int bg);
+	u32 getHOFS(const size_t bg);
+	u32 getVOFS(const size_t bg);
 
 	typedef u8 TBlendTable[32][32];
 	TBlendTable *blendTable;
@@ -865,7 +867,7 @@ namespace GPU_EXT
 void sprite1D(GPU *gpu, u16 l, u8 *dst, u8 *dst_alpha, u8 *typeTab, u8 *prioTab);
 void sprite2D(GPU *gpu, u16 l, u8 *dst, u8 *dst_alpha, u8 *typeTab, u8 *prioTab);
 
-extern const size sprSizeTab[4][4];
+extern const SpriteSize sprSizeTab[4][4];
 
 typedef struct
 {
@@ -882,8 +884,8 @@ void Screen_DeInit(void);
 
 extern MMU_struct MMU;
 
-void GPU_setVideoProp(GPU *gpu, u32 p);
-void GPU_setBGProp(GPU *gpu, u16 num, u16 p);
+void GPU_setVideoProp(GPU *gpu, const u32 ctrlBits);
+void GPU_setBGProp(GPU *gpu, const size_t num, const u16 ctrlBits);
 
 void GPU_setBLDCNT(GPU *gpu, u16 v);
 void GPU_setBLDY(GPU *gpu, u16 v);
@@ -907,7 +909,7 @@ inline void GPU_setWIN0_V(GPU *gpu, u16 val) { gpu->WIN0V0 = val >> 8; gpu->WIN0
 inline void GPU_setWIN0_V0(GPU *gpu, u8 val) { gpu->WIN0V0 = val; }
 inline void GPU_setWIN0_V1(GPU *gpu, u8 val) { gpu->WIN0V1 = val; }
 
-inline void GPU_setWIN1_H(GPU *gpu, u16 val) {gpu->WIN1H0 = val >> 8; gpu->WIN1H1 = val&0xFF;  gpu->need_update_winh[1] = true; }
+inline void GPU_setWIN1_H(GPU *gpu, u16 val) { gpu->WIN1H0 = val >> 8; gpu->WIN1H1 = val&0xFF;  gpu->need_update_winh[1] = true; }
 inline void GPU_setWIN1_H0(GPU *gpu, u8 val) { gpu->WIN1H0 = val;  gpu->need_update_winh[1] = true; }
 inline void GPU_setWIN1_H1(GPU *gpu, u8 val) { gpu->WIN1H1 = val;  gpu->need_update_winh[1] = true; }
 

desmume/src/OGLRender.cpp
@@ -28,6 +28,13 @@
 #include "NDSSystem.h"
 #include "texcache.h"
 
+#ifdef ENABLE_SSE2
+#include <emmintrin.h>
+#endif
+
+#ifdef ENABLE_SSSE3
+#include <tmmintrin.h>
+#endif
 
 typedef struct
 {
@@ -885,6 +892,79 @@ void OpenGLRenderer::SetVersion(unsigned int major, unsigned int minor, unsigned
 	this->versionRevision = revision;
 }
 
+#if defined(ENABLE_SSE2) && defined(ENABLE_SSSE3) && defined(LOCAL_LE)
+Render3DError OpenGLRenderer::FlushFramebuffer(FragmentColor *__restrict dstRGBA6665, u16 *__restrict dstRGBA5551)
+{
+	// Convert from 32-bit BGRA8888 format to 32-bit RGBA6665 reversed format. OpenGL
+	// stores pixels using a flipped Y-coordinate, so this needs to be flipped back
+	// to the DS Y-coordinate.
+	
+	if ((this->_framebufferWidth % 4) == 0)
+	{
+		for (size_t y = 0, ir = 0, iw = ((this->_framebufferHeight - 1) * this->_framebufferWidth); y < this->_framebufferHeight; y++, iw -= (this->_framebufferWidth * 2))
+		{
+			for (size_t x = 0; x < this->_framebufferWidth; x+=4, ir+=4, iw+=4)
+			{
+				// Convert to RGBA6665
+				__m128i v = _mm_load_si128((__m128i *)(this->_framebufferColor + ir));
+				v = _mm_srli_epi32(v, 2);
+				
+				__m128i a = _mm_srli_epi32(v, 1); // Special handling for 5-bit alpha
+				a = _mm_and_si128(a, _mm_set1_epi32(0x1F000000));
+				
+				v = _mm_and_si128(v, _mm_set1_epi32(0x003F3F3F));
+				v = _mm_or_si128(v, a);
+				v = _mm_shuffle_epi8(v, _mm_set_epi8(15, 12, 13, 14, 11, 8, 9, 10, 7, 4, 5, 6, 3, 0, 1, 2)); // Swizzle RGBA to BGRA
+				_mm_store_si128((__m128i *)(dstRGBA6665 + iw), v);
+				
+				// Convert to RGBA5551
+				v = _mm_load_si128((__m128i *)(this->_framebufferColor + ir));
+				
+				__m128i b = _mm_and_si128(v, _mm_set1_epi32(0x000000F8));	// Read from R
+				b = _mm_slli_epi32(b, 7);									// Shift to B
+				
+				__m128i g = _mm_and_si128(v, _mm_set1_epi32(0x0000F800));	// Read from G
+				g = _mm_srli_epi32(g, 6);									// Shift in G
+				
+				__m128i r = _mm_and_si128(v, _mm_set1_epi32(0x00F80000));	// Read from B
+				r = _mm_srli_epi32(r, 19);									// Shift to R
+				
+				a = _mm_and_si128(v, _mm_set1_epi32(0xFF000000));			// Read from A
+				a = _mm_cmpgt_epi32(a, _mm_set1_epi32(0x00000000));			// Determine A
+				a = _mm_and_si128(a, _mm_set1_epi32(0x00008000));			// Mask to A
+				
+				v = b;
+				v = _mm_add_epi32(v, g);
+				v = _mm_add_epi32(v, r);
+				v = _mm_add_epi32(v, a);
+				
+				// All the colors are currently placed every other 16 bits, so we need to swizzle them
+				// to the lower 64 bits of our vector before we store them back to memory.
+				v = _mm_shuffle_epi8(v, _mm_set_epi8(15, 14, 11, 10, 7, 6, 3, 2, 13, 12, 9, 8, 5, 4, 1, 0));
+				_mm_storel_epi64((__m128i *)(dstRGBA5551 + iw), v);
+			}
+		}
+	}
+	else
+	{
+		for (size_t y = 0, ir = 0, iw = ((this->_framebufferHeight - 1) * this->_framebufferWidth); y < this->_framebufferHeight; y++, iw -= (this->_framebufferWidth * 2))
+		{
+			for (size_t x = 0; x < this->_framebufferWidth; x++, ir++, iw++)
+			{
+				dstRGBA6665[iw].color = BGRA8888_32Rev_To_RGBA6665_32Rev(this->_framebufferColor[ir].color);
+				dstRGBA5551[iw] = R5G5B5TORGB15((this->_framebufferColor[ir].b >> 3) & 0x1F,
+												(this->_framebufferColor[ir].g >> 3) & 0x1F,
+												(this->_framebufferColor[ir].r >> 3) & 0x1F) |
+												((this->_framebufferColor[ir].a == 0) ? 0x0000 : 0x8000);
+			}
+		}
+	}
+	
+	return RENDER3DERROR_NOERR;
+}
+
+#else // Code path where SSE2, SSSE3, or little-endian is not supported
+
 Render3DError OpenGLRenderer::FlushFramebuffer(FragmentColor *__restrict dstRGBA6665, u16 *__restrict dstRGBA5551)
 {
 	// Convert from 32-bit BGRA8888 format to 32-bit RGBA6665 reversed format. OpenGL
@@ -914,6 +994,8 @@ Render3DError OpenGLRenderer::FlushFramebuffer(FragmentColor *__restrict dstRGBA
 	
 	return RENDER3DERROR_NOERR;
 }
+
+#endif // defined(ENABLE_SSE2) && defined(ENABLE_SSSE3) && defined(LOCAL_LE)
 
 OpenGLRenderer_1_2::~OpenGLRenderer_1_2()
 {

desmume/src/matrix.h
@@ -120,30 +120,75 @@ FORCEINLINE s32 s32floor(double d)
 //-------------
 #ifdef ENABLE_SSE2
 
-FORCEINLINE void memset_u16_le(void* dst, const size_t length, u16 val)
+static void memset_u16(void *dst, const u16 val, const size_t length)
 {
-	u32 u32val;
-	//just for the endian safety
-	T1WriteWord((u8*)&u32val, 0, val);
-	T1WriteWord((u8*)&u32val, 2, val);
-	////const __m128i temp = _mm_set_epi32(u32val,u32val,u32val,u32val);
-	
 #if defined(__GNUC__) || defined(__INTEL_COMPILER)
-	const __m128i temp = _mm_set_epi32(u32val,u32val,u32val,u32val);
-	MACRODO_N(length/8,_mm_store_si128((__m128i*)((u8*)dst+(X)*16), temp));
+	__m128i *dst_vec128 = (__m128i *)dst;
+	const __m128i val_vec128 = _mm_set1_epi16(val);
+	const size_t length_vec128 = length / (sizeof(val_vec128) / sizeof(val));
+	//MACRODO_N(length_vec128, (dst_vec128[X] = val_vec128));
+	
+	for (size_t i = 0; i < length_vec128; i++)
+		dst_vec128[i] = val_vec128;
 #else
-	__m128 temp; temp.m128_i32[0] = u32val;
-	//MACRODO_N(length/8,_mm_store_si128((__m128i*)((u8*)dst+(X)*16), temp));
-	MACRODO_N(length/8,_mm_store_ps1((float*)((u8*)dst+(X)*16), temp));
+	const u32 val_u32 = ((u32)val << 16) | (u32)val;
+	__m128 val_vec128; val_vec128.m128_i32[0] = val_u32;
+	const size_t length_vec128 = length / (sizeof(val_vec128) / sizeof(val));
+	//MACRODO_N(length_vec128,_mm_store_si128((__m128i*)((u8*)dst+(X)*16), val_vec128));
+	MACRODO_N(length_vec128, _mm_store_ps1((float*)((u8*)dst+(X)*16), val_vec128));
+#endif
+}
+
+static void memset_u32(void *dst, const u32 val, const size_t length)
+{
+#if defined(__GNUC__) || defined(__INTEL_COMPILER)
+	__m128i *dst_vec128 = (__m128i *)dst;
+	const __m128i val_vec128 = _mm_set1_epi32(val);
+	const size_t length_vec128 = length / (sizeof(val_vec128) / sizeof(val));
+	//MACRODO_N(length_vec128, (dst_vec128[X] = val_vec128));
+	
+	for (size_t i = 0; i < length_vec128; i++)
+		dst_vec128[i] = val_vec128;
+#else
+	__m128 val_vec128; val_vec128.m128_i32[0] = val;
+	const size_t length_vec128 = length / (sizeof(val_vec128) / sizeof(val));
+	//MACRODO_N(length_vec128,_mm_store_si128((__m128i*)((u8*)dst+(X)*16), val_vec128));
+	MACRODO_N(length_vec128, _mm_store_ps1((float*)((u8*)dst+(X)*16), val_vec128));
 #endif
 }
 
 #else //no sse2
 
-static FORCEINLINE void memset_u16_le(void *dst, const size_t length, const u16 val)
+static void memset_u16(void *dst, const u16 val, const size_t length)
 {
+#ifdef HOST_64
+	u64 *dst_u64 = (u64 *)dst;
+	const u64 val_u64 = ((u64)val << 48) | ((u64)val << 32) | ((u64)val << 16) | (u64)val;
+	const size_t length_u64 = length / (sizeof(val_u64) / sizeof(val));
+	//MACRODO_N(length_u64, (dst_u64[X] = val_u64));
+	
+	for (size_t i = 0; i < length_u64; i++)
+		dst_u64[i] = val_u64;
+#else
 	for (size_t i = 0; i < length; i++)
-		T1WriteWord((u8*)dst, i << 1, val);
+		((u16 *)dst)[i] = val;
+#endif
+}
+
+static void memset_u32(void *dst, const u32 val, const size_t length)
+{
+#ifdef HOST_64
+	u64 *dst_u64 = (u64 *)dst;
+	const u64 val_u64 = ((u64)val << 32) | (u64)val;
+	const size_t length_u64 = length / (sizeof(val_u64) / sizeof(val));
+	//MACRODO_N(length_u64, (dst_u64[X] = val_u64));
+	
+	for (size_t i = 0; i < length_u64; i++)
+		dst_u64[i] = val_u64;
+#else
+	for (size_t i = 0; i < length; i++)
+		((u32 *)dst)[i] = val;
+#endif
 }
 
 #endif

desmume/src/types.h
@@ -61,12 +61,21 @@
 #endif
 
 #ifdef __GNUC__
-#ifdef __SSE__
-#define ENABLE_SSE
-#endif
-#ifdef __SSE2__
-#define ENABLE_SSE2
-#endif
+	#ifdef __SSE__
+		#define ENABLE_SSE
+	#endif
+
+	#ifdef __SSE2__
+		#define ENABLE_SSE2
+	#endif
+
+	#ifdef __SSE3__
+		#define ENABLE_SSE3
+	#endif
+
+	#ifdef __SSSE3__
+		#define ENABLE_SSSE3
+	#endif
 #endif
 
 #ifdef NOSSE
@@ -392,7 +401,7 @@ char (*BLAHBLAHBLAH( UNALIGNED T (&)[N] ))[N];
 
 
 //fairly standard for loop macros
-#define MACRODO1(TRICK,TODO) { const int X = TRICK; TODO; }
+#define MACRODO1(TRICK,TODO) { const size_t X = TRICK; TODO; }
 #define MACRODO2(X,TODO)   { MACRODO1((X),TODO)   MACRODO1(((X)+1),TODO) }
 #define MACRODO4(X,TODO)   { MACRODO2((X),TODO)   MACRODO2(((X)+2),TODO) }
 #define MACRODO8(X,TODO)   { MACRODO4((X),TODO)   MACRODO4(((X)+4),TODO) }

commit 063c1a83b8438cff12735bb502945abb38fea2d7

GPU: - Revert VRAM readback optimization. Fixes the battle sequence in Golden Sun: Dark Dawn. (Regression from r5208.)
git-svn-id: https://svn.code.sf.net/p/desmume/code/trunk@5209 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/GPU.cpp
@@ -185,11 +185,10 @@ GPU* GPU_Init(const GPUCoreID coreID)
 	gpu->bgPixels = NULL;
 	gpu->h_win[0] = NULL;
 	gpu->h_win[1] = NULL;
-	gpu->VRAMBuffer = NULL;
-
+	
 	GPU_Reset(gpu);
 	GPU_InitFadeColors();
-
+	
 	gpu->curr_win[0] = win_empty;
 	gpu->curr_win[1] = win_empty;
 	gpu->need_update_winh[0] = true;
@@ -197,7 +196,7 @@ GPU* GPU_Init(const GPUCoreID coreID)
 	gpu->setFinalColorBck_funcNum = 0;
 	gpu->setFinalColor3d_funcNum = 0;
 	gpu->setFinalColorSpr_funcNum = 0;
-
+	
 	return gpu;
 }
 
@@ -209,7 +208,6 @@ void GPU_Reset(GPU *gpu)
 	// It would be better if we were more precise about GPU resets.
 	const GPUCoreID currentCoreID = gpu->core;
 	u16 *currentTempScanlineBuffer = gpu->tempScanlineBuffer;
-	u16 *currentVRAMBuffer = gpu->VRAMBuffer;
 	u8 *currentBGPixels = gpu->bgPixels;
 	u8 *currentHWin0 = gpu->h_win[0];
 	u8 *currentHWin1 = gpu->h_win[1];
@@ -218,14 +216,12 @@ void GPU_Reset(GPU *gpu)
 	
 	gpu->core = currentCoreID;
 	gpu->tempScanlineBuffer = currentTempScanlineBuffer;
-	gpu->VRAMBuffer = currentVRAMBuffer;
 	gpu->bgPixels = currentBGPixels;
 	gpu->h_win[0] = currentHWin0;
 	gpu->h_win[1] = currentHWin1;
 	
 	// Clear the separate memory blocks that weren't cleared in the last memset()
 	if (gpu->tempScanlineBuffer != NULL) memset(gpu->tempScanlineBuffer, 0, _gpuFramebufferWidth * _gpuLargestDstLineCount * sizeof(u16));
-	if (gpu->VRAMBuffer != NULL) memset(gpu->VRAMBuffer, 0, _gpuFramebufferWidth * _gpuFramebufferHeight * sizeof(u16));
 	if (gpu->bgPixels != NULL) memset(gpu->bgPixels, 0, _gpuFramebufferWidth * _gpuLargestDstLineCount * 4 * sizeof(u8));
 	if (gpu->h_win[0] != NULL) memset(gpu->h_win[0], 0, _gpuFramebufferWidth * sizeof(u8));
 	if (gpu->h_win[1] != NULL) memset(gpu->h_win[1], 0, _gpuFramebufferWidth * sizeof(u8));
@@ -278,9 +274,6 @@ void GPU_DeInit(GPU *gpu)
 	gpu->h_win[0] = NULL;
 	free(gpu->h_win[1]);
 	gpu->h_win[1] = NULL;
-	
-	free(gpu->VRAMBuffer);
-	gpu->VRAMBuffer = NULL;
 	
 	free(gpu);
 	gpu = NULL;
@@ -412,38 +405,8 @@ void GPU_setVideoProp(GPU *gpu, const u32 ctrlBits)
 			break;
 			
 		case GPUDisplayMode_VRAM: // Display framebuffer
-		{
 			gpu->VRAMaddr = (u16 *)((u8 *)MMU.ARM9_LCD + (gpu->vramBlock * ADDRESS_STEP_128KB));
-			
-			if (_gpuFramebufferWidth == GPU_FRAMEBUFFER_NATIVE_WIDTH && _gpuFramebufferHeight == GPU_FRAMEBUFFER_NATIVE_HEIGHT)
-			{
-#ifdef LOCAL_BE
-				for (size_t i = 0; i < GPU_FRAMEBUFFER_NATIVE_WIDTH * GPU_FRAMEBUFFER_NATIVE_HEIGHT; i++)
-				{
-					gpu->VRAMBuffer[i] = LE_TO_LOCAL_16(gpu->VRAMaddr[i]);
-				}
-#else
-				memcpy(gpu->VRAMBuffer, gpu->VRAMaddr, GPU_FRAMEBUFFER_NATIVE_WIDTH * GPU_FRAMEBUFFER_NATIVE_HEIGHT * sizeof(u16));
-#endif
-			}
-			else
-			{
-				for (size_t y = 0; y < GPU_FRAMEBUFFER_NATIVE_HEIGHT; y++)
-				{
-					for (size_t x = 0; x < GPU_FRAMEBUFFER_NATIVE_WIDTH; x++)
-					{
-						for (size_t l = 0; l < _gpuDstLineCount[y]; l++)
-						{
-							for (size_t p = 0; p < _gpuDstPitchCount[x]; p++)
-							{
-								gpu->VRAMBuffer[((_gpuDstLineIndex[y] + l) * _gpuFramebufferWidth) + (_gpuDstPitchIndex[x] + p)] = LE_TO_LOCAL_16(gpu->VRAMaddr[(y * GPU_FRAMEBUFFER_NATIVE_WIDTH) + x]);
-							}
-						}
-					}
-				}
-			}
 			break;
-		}
 			
 		case GPUDisplayMode_MainMemory: // Display from Main RAM
 			// nothing to be done here
@@ -2044,9 +2007,6 @@ void GPU_SetFramebufferSize(size_t w, size_t h)
 	MainScreen.gpu->bgPixels = (u8 *)realloc(MainScreen.gpu->bgPixels, w * _gpuLargestDstLineCount * 4 * sizeof(u8)); // yes indeed, this is oversized. map debug tools try to write to it
 	SubScreen.gpu->bgPixels = (u8 *)realloc(SubScreen.gpu->bgPixels, w * _gpuLargestDstLineCount * 4 * sizeof(u8)); // yes indeed, this is oversized. map debug tools try to write to it
 	
-	MainScreen.gpu->VRAMBuffer = (u16 *)realloc(MainScreen.gpu->VRAMBuffer, w * h * sizeof(u16));
-	SubScreen.gpu->VRAMBuffer = (u16 *)realloc(SubScreen.gpu->VRAMBuffer, w * h * sizeof(u16));
-	
 	const size_t windowBufferSize = w * sizeof(u8);
 	const u8 *oldWinEmptyPtr = win_empty;
 	win_empty = (u8 *)realloc(win_empty, windowBufferSize);
@@ -2710,7 +2670,33 @@ void GPU_RenderLine(NDS_Screen *screen, const u16 l, bool skip)
 			break;
 			
 		case GPUDisplayMode_VRAM: // Display vram framebuffer
-			memcpy(dstLine, gpu->VRAMBuffer + (dstLineIndex * _gpuFramebufferWidth), _gpuFramebufferWidth * dstLineCount * sizeof(u16));
+		{
+			const u16 *src = (u16 *)gpu->VRAMaddr + (l * GPU_FRAMEBUFFER_NATIVE_WIDTH);
+#ifdef LOCAL_BE
+			for (size_t i = 0; i < GPU_FRAMEBUFFER_NATIVE_WIDTH; i++)
+			{
+				dstLine[i] = LE_TO_LOCAL_16(src[i]);
+			}
+#else
+			if (_gpuFramebufferWidth == GPU_FRAMEBUFFER_NATIVE_WIDTH && _gpuFramebufferHeight == GPU_FRAMEBUFFER_NATIVE_HEIGHT)
+			{
+				memcpy(dstLine, src, GPU_FRAMEBUFFER_NATIVE_WIDTH * sizeof(u16));
+			}
+			else
+			{
+				for (size_t x = 0; x < GPU_FRAMEBUFFER_NATIVE_WIDTH; x++)
+				{
+					for (size_t line = 0; line < _gpuDstLineCount[l]; line++)
+					{
+						for (size_t p = 0; p < _gpuDstPitchCount[x]; p++)
+						{
+							dstLine[(line * _gpuFramebufferWidth) + (x + p)] = LE_TO_LOCAL_16(src[x]);
+						}
+					}
+				}
+			}
+#endif
+		}
 			break;
 			
 		case GPUDisplayMode_MainMemory: // Display memory FIFO

desmume/src/GPU.h
@@ -661,7 +661,6 @@ struct GPU
 	GPUDisplayMode dispMode;
 	u8 vramBlock;
 	u16 *VRAMaddr;
-	u16 *VRAMBuffer;
 
 	//FIFO	fifo;
 