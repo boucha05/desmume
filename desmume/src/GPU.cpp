boucha05/desmume

commit efb2819ff6ba7b690bfa6cadc7c956e9bee4a878

GPU: - Auto-resolving the native framebuffer is now only performed if the frame isn’t skipped. - Add some callback routines for the beginning and ending of rendering a frame, and for the beginning and ending of rendering the 3D layer.
git-svn-id: https://svn.code.sf.net/p/desmume/code/trunk@5288 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/GPU.cpp
@@ -723,7 +723,7 @@ void GPUEngineBase::_RenderLine_Clear(const u16 clearColor, const u16 l, u16 *ds
 }
 
 template<bool ISCUSTOMRENDERINGNEEDED>
-void GPUEngineBase::RenderLine(const u16 l, bool isFrameSkipRequested)
+void GPUEngineBase::RenderLine(const u16 l)
 {
 	
 }
@@ -2710,7 +2710,7 @@ void GPUEngineA::SetCustomFramebufferSize(size_t w, size_t h)
 }
 
 template<bool ISCUSTOMRENDERINGNEEDED>
-void GPUEngineA::RenderLine(const u16 l, bool isFrameSkipRequested)
+void GPUEngineA::RenderLine(const u16 l)
 {
 	const NDSDisplayInfo &dispInfo = GPU->GetDisplayInfo();
 	const size_t dstLineWidth = (ISCUSTOMRENDERINGNEEDED) ? dispInfo.customWidth : GPU_FRAMEBUFFER_NATIVE_WIDTH;
@@ -3808,6 +3808,9 @@ void GPUEngineB::_RenderLine_Layer(const u16 l, u16 *dstColorLine, const size_t 
 
 GPUSubsystem::GPUSubsystem()
 {
+	_defaultEventHandler = new GPUEventHandlerDefault;
+	_event = _defaultEventHandler;
+	
 	gfx3d_init();
 	
 	_engineMain = GPUEngineA::Allocate();
@@ -3867,6 +3870,8 @@ GPUSubsystem::~GPUSubsystem()
 	_engineSub->FinalizeAndDeallocate();
 	
 	gfx3d_deinit();
+	
+	delete _defaultEventHandler;
 }
 
 GPUSubsystem* GPUSubsystem::Allocate()
@@ -3878,6 +3883,16 @@ void GPUSubsystem::FinalizeAndDeallocate()
 {
 	this->~GPUSubsystem();
 	free_aligned(this);
+}
+
+void GPUSubsystem::SetEventHandler(GPUEventHandler *eventHandler)
+{
+	this->_event = eventHandler;
+}
+
+GPUEventHandler* GPUSubsystem::GetEventHandler()
+{
+	return this->_event;
 }
 
 void GPUSubsystem::Reset()
@@ -4184,6 +4199,7 @@ void GPUSubsystem::RenderLine(const u16 l, bool isFrameSkipRequested)
 	if (l == 0)
 	{
 		CurrentRenderer->RenderFinish();
+		this->_event->DidFrameBegin();
 		this->UpdateVRAM3DUsageProperties();
 		
 		// Clear displays to black if they are turned off by the user.
@@ -4215,11 +4231,11 @@ void GPUSubsystem::RenderLine(const u16 l, bool isFrameSkipRequested)
 		{
 			if (this->_engineMain->isCustomRenderingNeeded)
 			{
-				this->_engineMain->RenderLine<true>(l, isFrameSkipRequested);
+				this->_engineMain->RenderLine<true>(l);
 			}
 			else
 			{
-				this->_engineMain->RenderLine<false>(l, isFrameSkipRequested);
+				this->_engineMain->RenderLine<false>(l);
 			}
 		}
 		
@@ -4249,16 +4265,17 @@ void GPUSubsystem::RenderLine(const u16 l, bool isFrameSkipRequested)
 			{
 				this->_engineSub->ApplyMasterBrightness<false>();
 			}
-		}
-		
-		if (this->_willAutoResolveToCustomBuffer)
-		{
-			this->_engineMain->ResolveToCustomFramebuffer();
-			this->_engineSub->ResolveToCustomFramebuffer();
+			
+			if (this->_willAutoResolveToCustomBuffer)
+			{
+				this->_engineMain->ResolveToCustomFramebuffer();
+				this->_engineSub->ResolveToCustomFramebuffer();
+			}
 		}
 		
 		this->_engineMain->FramebufferPostprocess();
 		this->_engineSub->FramebufferPostprocess();
+		this->_event->DidFrameEnd(isFrameSkipRequested);
 	}
 }
 

desmume/src/OGLRender.cpp
@@ -2720,11 +2720,13 @@ Render3DError OpenGLRenderer_1_2::RenderFinish()
 {
 	if (!this->_pixelReadNeedsFinish)
 	{
+		GPU->GetEventHandler()->DidRender3DEnd();
 		return OGLERROR_NOERR;
 	}
 	
 	if(!BEGINGL())
 	{
+		GPU->GetEventHandler()->DidRender3DEnd();
 		return OGLERROR_BEGINGL_FAILED;
 	}
 	
@@ -2747,6 +2749,7 @@ Render3DError OpenGLRenderer_1_2::RenderFinish()
 	this->FlushFramebuffer(GPU->GetEngineMain()->Get3DFramebufferRGBA6665(), GPU->GetEngineMain()->Get3DFramebufferRGBA5551());
 	this->_pixelReadNeedsFinish = false;
 	
+	GPU->GetEventHandler()->DidRender3DEnd();
 	return OGLERROR_NOERR;
 }
 
@@ -3287,11 +3290,13 @@ Render3DError OpenGLRenderer_1_5::RenderFinish()
 {
 	if (!this->_pixelReadNeedsFinish)
 	{
+		GPU->GetEventHandler()->DidRender3DEnd();
 		return OGLERROR_NOERR;
 	}
 	
 	if(!BEGINGL())
 	{
+		GPU->GetEventHandler()->DidRender3DEnd();
 		return OGLERROR_BEGINGL_FAILED;
 	}
 	
@@ -3314,6 +3319,7 @@ Render3DError OpenGLRenderer_1_5::RenderFinish()
 	this->FlushFramebuffer(GPU->GetEngineMain()->Get3DFramebufferRGBA6665(), GPU->GetEngineMain()->Get3DFramebufferRGBA5551());
 	this->_pixelReadNeedsFinish = false;
 	
+	GPU->GetEventHandler()->DidRender3DEnd();
 	return OGLERROR_NOERR;
 }
 
@@ -4077,11 +4083,13 @@ Render3DError OpenGLRenderer_2_1::RenderFinish()
 {
 	if (!this->_pixelReadNeedsFinish)
 	{
+		GPU->GetEventHandler()->DidRender3DEnd();
 		return OGLERROR_NOERR;
 	}
 	
 	if(!BEGINGL())
 	{
+		GPU->GetEventHandler()->DidRender3DEnd();
 		return OGLERROR_BEGINGL_FAILED;
 	}
 	
@@ -4097,5 +4105,6 @@ Render3DError OpenGLRenderer_2_1::RenderFinish()
 	this->FlushFramebuffer(GPU->GetEngineMain()->Get3DFramebufferRGBA6665(), GPU->GetEngineMain()->Get3DFramebufferRGBA5551());
 	this->_pixelReadNeedsFinish = false;
 	
+	GPU->GetEventHandler()->DidRender3DEnd();
 	return OGLERROR_NOERR;
 }

desmume/src/gfx3d.cpp
@@ -559,7 +559,6 @@ void gfx3d_init()
 	
 	makeTables();
 	Render3D_Init();
-	gfx3d_reset();
 }
 
 void gfx3d_deinit()

desmume/src/rasterize.cpp
@@ -1933,6 +1933,7 @@ Render3DError SoftRasterizerRenderer::Render(const GFX3D &engine)
 {
 	Render3DError error = RENDER3DERROR_NOERR;
 	
+	GPU->GetEventHandler()->DidRender3DBegin();
 	error = this->BeginRender(engine);
 	if (error != RENDER3DERROR_NOERR)
 	{
@@ -1970,6 +1971,7 @@ Render3DError SoftRasterizerRenderer::RenderFinish()
 {
 	if (!this->_renderGeometryNeedsFinish)
 	{
+		GPU->GetEventHandler()->DidRender3DEnd();
 		return RENDER3DERROR_NOERR;
 	}
 	
@@ -2005,6 +2007,7 @@ Render3DError SoftRasterizerRenderer::RenderFinish()
 	
 	this->FlushFramebuffer(GPU->GetEngineMain()->Get3DFramebufferRGBA6665(), GPU->GetEngineMain()->Get3DFramebufferRGBA5551());
 	
+	GPU->GetEventHandler()->DidRender3DEnd();
 	return RENDER3DERROR_NOERR;
 }
 

desmume/src/render3D.cpp
@@ -437,6 +437,7 @@ Render3DError Render3D::Render(const GFX3D &engine)
 {
 	Render3DError error = RENDER3DERROR_NOERR;
 	
+	GPU->GetEventHandler()->DidRender3DBegin();
 	error = this->BeginRender(engine);
 	if (error != RENDER3DERROR_NOERR)
 	{
@@ -465,6 +466,7 @@ Render3DError Render3D::Render(const GFX3D &engine)
 
 Render3DError Render3D::RenderFinish()
 {
+	GPU->GetEventHandler()->DidRender3DEnd();
 	return RENDER3DERROR_NOERR;
 }
 

desmume/src/GPU.h
@@ -1229,7 +1229,7 @@ public:
 	
 	template<GPUEngineID ENGINEID> void ParseAllRegisters();
 	
-	template<bool ISCUSTOMRENDERINGNEEDED> void RenderLine(const u16 l, bool isFrameSkipRequested);
+	template<bool ISCUSTOMRENDERINGNEEDED> void RenderLine(const u16 l);
 	void FramebufferPostprocess();
 	
 	bool isCustomRenderingNeeded;
@@ -1337,7 +1337,7 @@ public:
 	
 	template<GPULayerID LAYERID, bool ISDEBUGRENDER, bool MOSAIC, bool ISCUSTOMRENDERINGNEEDED> void _LineLarge8bpp(u16 *dstColorLine, const u16 lineIndex);
 		
-	template<bool ISCUSTOMRENDERINGNEEDED> void RenderLine(const u16 l, bool isFrameSkipRequested);
+	template<bool ISCUSTOMRENDERINGNEEDED> void RenderLine(const u16 l);
 	void FramebufferPostprocess();
 };
 
@@ -1376,11 +1376,35 @@ public:
 	void SetEngineByID(const GPUEngineID theID);
 };
 
+class GPUEventHandler
+{
+public:
+	virtual void DidFrameBegin() = 0;
+	virtual void DidFrameEnd(bool isFrameSkipped) = 0;
+	virtual void DidRender3DBegin() = 0;
+	virtual void DidRender3DEnd() = 0;
+};
+
+// All of the default event handler methods should do nothing.
+// If a subclass doesn't need to override every method, then it might be easier
+// if you subclass GPUEventHandlerDefault instead of GPUEventHandler.
+class GPUEventHandlerDefault : public GPUEventHandler
+{
+public:
+	virtual void DidFrameBegin() {};
+	virtual void DidFrameEnd(bool isFrameSkipped) {};
+	virtual void DidRender3DBegin() {};
+	virtual void DidRender3DEnd() {};
+};
+
 class GPUSubsystem
 {
 private:
 	GPUSubsystem();
 	~GPUSubsystem();
+	
+	GPUEventHandlerDefault *_defaultEventHandler;
+	GPUEventHandler *_event;
 	
 	GPUEngineA *_engineMain;
 	GPUEngineB *_engineSub;
@@ -1400,6 +1424,9 @@ private:
 public:
 	static GPUSubsystem* Allocate();
 	void FinalizeAndDeallocate();
+	
+	void SetEventHandler(GPUEventHandler *eventHandler);
+	GPUEventHandler* GetEventHandler();
 	
 	void Reset();
 	VRAM3DUsageProperties& GetVRAM3DUsageProperties();

desmume/src/cocoa/cocoa_GPU.h
@@ -19,6 +19,9 @@
 #include <pthread.h>
 #include <libkern/OSAtomic.h>
 
+
+class GPUEventHandlerOSX;
+
 @interface CocoaDSGPU : NSObject
 {
 	UInt32 gpuStateFlags;
@@ -26,13 +29,14 @@
 	BOOL isCPUCoreCountAuto;
 	
 	OSSpinLock spinlockGpuState;
-	pthread_rwlock_t *rwlockProducer;
+	GPUEventHandlerOSX *gpuEvent;
 }
 
 @property (assign) UInt32 gpuStateFlags;
 @property (assign) NSSize gpuDimensions;
 @property (assign) NSUInteger gpuScale;
-@property (assign) pthread_rwlock_t *rwlockProducer;
+@property (readonly) pthread_rwlock_t *gpuFrameRWLock;
+@property (assign) NSMutableArray *outputList;
 
 @property (assign) BOOL layerMainGPU;
 @property (assign) BOOL layerMainBG0;

commit 0501b2b69f564f4268d7be6b3f03eb36ffbd3726

Cocoa Port: - Fix occasional crash that can occur when switching 3D rendering engines. (Regression from r5288.)
git-svn-id: https://svn.code.sf.net/p/desmume/code/trunk@5382 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/GPU.cpp
@@ -6100,7 +6100,11 @@ void GPUSubsystem::SetCustomFramebufferSize(size_t w, size_t h, u16 *clientNativ
 	this->_engineMain->SetCustomFramebufferSize(w, h);
 	this->_engineSub->SetCustomFramebufferSize(w, h);
 	BaseRenderer->SetFramebufferSize(w, h); // Since BaseRenderer is persistent, we need to update this manually.
-	CurrentRenderer->SetFramebufferSize(w, h);
+	
+	if (CurrentRenderer != BaseRenderer)
+	{
+		CurrentRenderer->SetFramebufferSize(w, h);
+	}
 	
 	if (this->_displayInfo.didPerformCustomRender[NDSDisplayID_Main])
 	{

desmume/src/OGLRender.cpp
@@ -786,8 +786,9 @@ static void OpenGLRendererDestroy()
 	
 	if (CurrentRenderer != BaseRenderer)
 	{
-		delete (OpenGLRenderer *)CurrentRenderer;
+		OpenGLRenderer *oldRenderer = (OpenGLRenderer *)CurrentRenderer;
 		CurrentRenderer = BaseRenderer;
+		delete oldRenderer;
 	}
 	
 	ENDGL();

desmume/src/cocoa/cocoa_GPU.mm
@@ -49,6 +49,7 @@ private:
 	pthread_mutex_t *_mutexOutputList;
 	NSMutableArray *_cdsOutputList;
 	bool _isRender3DLockHeld;
+	bool _isIn3DRender;
 	
 public:
 	GPUEventHandlerOSX();
@@ -723,6 +724,7 @@ public:
 GPUEventHandlerOSX::GPUEventHandlerOSX()
 {
 	_isRender3DLockHeld = false;
+	_isIn3DRender = false;
 	_mutexOutputList = NULL;
 	pthread_rwlock_init(&_rwlockFrame, NULL);
 	pthread_mutex_init(&_mutex3DRender, NULL);
@@ -778,12 +780,17 @@ void GPUEventHandlerOSX::DidFrameEnd(bool isFrameSkipped)
 
 void GPUEventHandlerOSX::DidRender3DBegin()
 {
+	this->_isIn3DRender = true;
 	this->Render3DLock();
 }
 
 void GPUEventHandlerOSX::DidRender3DEnd()
 {
-	this->Render3DUnlock();
+	if (this->_isIn3DRender)
+	{
+		this->Render3DUnlock();
+		this->_isIn3DRender = false;
+	}
 }
 
 void GPUEventHandlerOSX::FramebufferLockWrite()
@@ -811,8 +818,8 @@ void GPUEventHandlerOSX::Render3DUnlock()
 {
 	if (this->_isRender3DLockHeld)
 	{
-		pthread_mutex_unlock(&this->_mutex3DRender);
 		this->_isRender3DLockHeld = false;
+		pthread_mutex_unlock(&this->_mutex3DRender);
 	}
 }
 

desmume/src/gfx3d.cpp
@@ -2314,6 +2314,7 @@ void gfx3d_VBlankEndSignal(bool skipFrame)
 	if (!CommonSettings.showGpu.main)
 	{
 		memset(GPU->GetEngineMain()->Get3DFramebufferRGBA6665(), 0, GPU->GetCustomFramebufferWidth() * GPU->GetCustomFramebufferHeight() * sizeof(FragmentColor));
+		memset(GPU->GetEngineMain()->Get3DFramebufferRGBA5551(), 0, GPU->GetCustomFramebufferWidth() * GPU->GetCustomFramebufferHeight() * sizeof(u16));
 		return;
 	}
 	

desmume/src/rasterize.cpp
@@ -1146,13 +1146,14 @@ static void SoftRasterizerRendererDestroy()
 	if (CurrentRenderer != BaseRenderer)
 	{
 #if defined(ENABLE_SSSE3)
-		delete (SoftRasterizerRenderer_SSSE3 *)CurrentRenderer;
+		SoftRasterizerRenderer_SSSE3 *oldRenderer = (SoftRasterizerRenderer_SSSE3 *)CurrentRenderer;
 #elif defined(ENABLE_SSE2)
-		delete (SoftRasterizerRenderer_SSE2 *)CurrentRenderer;
+		SoftRasterizerRenderer_SSE2 *oldRenderer = (SoftRasterizerRenderer_SSE2 *)CurrentRenderer;
 #else
-		delete (SoftRasterizerRenderer *)CurrentRenderer;
+		SoftRasterizerRenderer *oldRenderer = (SoftRasterizerRenderer *)CurrentRenderer;
 #endif
 		CurrentRenderer = BaseRenderer;
+		delete oldRenderer;
 	}
 }
 

desmume/src/render3D.cpp
@@ -119,8 +119,9 @@ void Render3DBaseDestroy()
 {
 	if (CurrentRenderer != BaseRenderer)
 	{
-		delete CurrentRenderer;
+		Render3D *oldRenderer = CurrentRenderer;
 		CurrentRenderer = BaseRenderer;
+		delete oldRenderer;
 	}
 }
 